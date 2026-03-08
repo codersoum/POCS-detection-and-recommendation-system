86bb1e11765f3fde5d2e5ee61fe3d34a280012f6
codersoum <22h51a0558@cmrcet.ac.in>
2026-03-08 14:27:39 +0530

    updated version

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import google.generativeai as genai
 import os
+from pages.extraction.report_extraction import parse_interpret
 from dotenv import load_dotenv
-
 load_dotenv()
+# from import report_extraction
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 SYSTEM_PROMPT = """You are a healthcare AI assistant.
@@ -15,7 +16,7 @@ Never answer non-healthcare topics."""
 
 def main():
     if not st.session_state.get("authenticated") or not st.session_state.get("name"):
-         st.warning("Please login back to access the POCS detection system")
+         st.warning("Please login back to access the PCOS recommendation system")
          st.stop()
         # -------- Setup --------
     st.title("🤖 PCOS Chatbot")
@@ -24,25 +25,50 @@ def main():
     if "chat" not in st.session_state:
             model = genai.GenerativeModel("gemini-2.5-flash-lite",system_instruction=SYSTEM_PROMPT)
             st.session_state.chat = model.start_chat(history=[])
+    if "conversations" not in st.session_state:
+         st.session_state.conversations=[]
 
 # -------- Display History --------
-    for msg in st.session_state.chat.history:
-        with st.chat_message(msg.role):
-             st.markdown(msg.parts[0].text)
-
+    for message in st.session_state.conversations:
+         with st.chat_message(message["role"]):
+              st.markdown(message["content"])
+         
 # -------- User Input --------
-    user_input = st.chat_input("Ask a healthcare question...")
+    filename=st.file_uploader("Upload a file ",type=["pdf","text"],accept_multiple_files=False)
+    interpretations=[]
 
-    if user_input:
-        with st.chat_message("user"):
-             st.markdown(user_input)
-
-        response = st.session_state.chat.send_message(user_input)
-
+    # parsing file and extract meaningful sentences .Send these sentences as prompts to model and get the response. Display the response in the chat interface.
+    if  filename is not None:
+        st.session_state.conversations.append({
+             "role":"user",
+             "content":f'Uploaded file: {filename.name}'
+        })
+        with st.spinner("Extracting information from the file"):
+            interpretations= parse_interpret(filename)
+        #with st.chat_message("user"):
+        #     for lines in interpretations:
+        #           st.markdown(lines)
+        prompt=f'''
+        You are a healthcare AI assistant specialized in PCOS.Below are the interpretations of patient {st.session_state.name}'s report, 
+        '''
+        for line in interpretations:
+             prompt+=line+"\n"
+        prompt+="""Based on the above interpretations,
+        
+        1.Assess the likelihood of the patient having PCOS.
+        2.Describe the symptoms that the patient is facing
+        3.Suggest few lifestyle recommendations
+        Please provide a short and concise answer and always use 'You' to refer to the patient in your response."
+        """
+        response = st.session_state.chat.send_message(prompt)
+        st.session_state.conversations.append({
+             "role": "assistant",
+             "content": response.text
+        })
         with st.chat_message("assistant"):
-             st.markdown(response.text)
+              st.markdown(response.text)
 
-# -------- Clear Chat --------
+#-------- Clear Chat --------
     if st.sidebar.button("Clear Chat"):
         st.session_state.chat = genai.GenerativeModel("gemini-2.5-flash-lite",system_instruction=SYSTEM_PROMPT).start_chat(history=[])
         st.rerun()
